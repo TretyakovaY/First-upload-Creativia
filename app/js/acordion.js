bfc1aebd2307fd6d1d4178ff08ed602ecc0a3605
TretyakovaY <tretyakova.yuliia@gmail.com>
2018-12-02 16:36:15 +0200

    Part Portfolio width errors

--- app/js/acordion.js
+++ app/js/acordion.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-	console.log('test')
+  console.log('test')
   $('.p-icon').on('click', function(){
       $(this).toggleClass('active');
   });
     $('.clear').on('click', function(){
+      $(this).toggleClass('active');
+  });
+     $('.accordion-part').on('click', function(){
       $(this).toggleClass('active');
   });
      $('.p-icon').on('click', function(){
