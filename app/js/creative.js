75426a0a1aad53d3d6191fa75f4fb0cc91b54817
TretyakovaY <tretyakova.yuliia@gmail.com>
2018-12-25 00:04:15 +0200

    Creativia with Clients

--- app/js/creative.js
+++ app/js/creative.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
   	  speed: 1000,
   	  cssEase: 'linear'
     });
+  $('.client-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+  	  arrows: false,
+  	  fade: true,
+  	  speed: 1000,
+  	  dots: true,
+  	  autoplay: true,
+  	  autoplaySpeed: 2000,
+    });
  });
